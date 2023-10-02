4f31d2eeed095a68a6cbc4dbf7bef0f525db63a2
MaksimK30 <makskuzmih@mail.ru>
2023-10-02 14:17:25 +0300

    first commit

---
+++ Avto.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace AvtoIsay
+{
+    internal class Avto
+    {
+        string num;
+        float amount;
+        float consumption;
+        int speed = 80;
+        int runway;
+        int time;
+        public void info(string num, float amount, float consumption)
+        {
+            this.num = num;
+            this.amount = amount;
+            this.consumption = consumption;
+        }
+        public void zapravka(float top)
+        {
+            amount = top;
+        }
+        public void move(int km)
+        {
+            time = speed / km;
+            amount =- time * consumption;
+            if (amount < 0)
+            {
+                Console.WriteLine("Мы не доедем. Есть заправка?");
+            }
+            else if (amount >= 0) 
+            {
+                runway = km;
+            }
+        }
+        public void result()
+        {
+            Console.WriteLine(amount);
+            Console.WriteLine(runway);
+        }
+        //public void slowdown()
+        //{
+
+        //}
+        //public double accel()
+        //{
+        //    double a;
+        //    a = speed / 0.00556;
+        //    do
+        //    {
+        //        a = +a;
+        //        time++;
+        //    }
+        //    while (a < time) ;
+        //    return time;
+        //}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using AvtoIsay;
+class program
+{
+    static void Main()
+    {
+        Random rand = new Random();
+        Avto car = new Avto();
+        car.info("1", 50, 7);
+        car.zapravka(50);
+        car.move(rand.Next(1, 500));
+        //Avto[] cars = new Avto[5];
+        //for (int i = 0; i < cars.Length; i++)
+        //{
+        //    cars[i] = new Avto();
+        //}
+        car.result();
+
+    }
+}
